62519a6125dc9ed786884f8bdbfcf0c26a7f38fa
Erica Winne <erica.winne@powerhrg.com>
2021-05-23 21:34:29 -0400

    first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(drivers.length-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fareQuintupler){
+    return function(){
+        return fareQuintupler * 5
+    }
+}
+function fareDoubler(fare){
+    return fare * 2
+}
+
+function fareTripler(fareTrip){
+    return fareTrip * 3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, name){
+    return name(arrayOfDrivers)
+}
